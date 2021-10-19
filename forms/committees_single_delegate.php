81196ad9b56bdaf8c0df2f148cbddc2054a9ee71
pranab1999 <impranabdas99@gmail.com>
2021-10-19 19:09:52 +0530

    added new committees and updated countries

--- forms/committees_single_delegate.php
+++ forms/committees_single_delegate.php
@@ -3,7 +3,7 @@
 <option>United nations Human Rights Council (UNHRC)</option>
 <option>Intergovernmental Panel on Climate Change (IPCC)</option>
 <option>United Nations Commission on the Status of Women (UNCSW)</option>
-<option>United Nations Children's Fund (UNICEF)</option>
+<option>United Nations Childrens Fund (UNICEF)</option>
 <option>United Nations Office on Drugs and Crime (UNODC)</option>
 <option>World Bank</option>
 <option>United Nations Security Council (UNSC)</option>
