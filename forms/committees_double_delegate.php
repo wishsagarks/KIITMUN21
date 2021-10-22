16b9c6d85679184a0f43bbcad5f28501db8682a1
Pranab Das <55971762+pranab99@users.noreply.github.com>
2021-10-22 13:57:34 +0530

    Update committees_double_delegate.php

--- forms/committees_double_delegate.php
+++ forms/committees_double_delegate.php
@@ -1,4 +1,4 @@
-<option>World health assembly (WHA)</option>
+<option>World Health Assembly (WHA)</option>
 <option>United Nations Children's Fund (UNICEF)</option>
 <option>United nations Human Rights Council (UNHRC)</option>
 <option>United Nations Office on Drugs and Crime (UNODC)</option>
