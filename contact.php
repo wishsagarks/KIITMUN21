46232259fd436f0d426595a99b825ea223a4bc17
Pranab Das <55971762+pranab99@users.noreply.github.com>
2021-11-16 16:30:49 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -183,7 +183,7 @@
         </section>
        
        
-        <div align="center" style="width:80%;">
+        <div align="center" style="width:80%;margin:auto;">
             <div data-tf-widget="onlaT4fj" style="width:100%;height:500px;"></div><script src="//embed.typeform.com/next/embed.js"></script> 
              <br>
             <p><i>For further queries, please feel free to contact <b>+91 9163299764,+91 7873252777</b></i></p>
